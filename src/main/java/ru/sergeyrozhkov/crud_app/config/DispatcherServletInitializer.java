645dca6519a84de4e21f35922c3777f96bdee0d2
Sergey <borzsonite@gmail.com>
2021-09-10 07:13:59 +0300

    html issue

---
+++ src/main/java/ru/sergeyrozhkov/crud_app/config/DispatcherServletInitializer.java
@@ -0,0 +1,20 @@
+package ru.sergeyrozhkov.crud_app.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class DispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {ru.sergeyrozhkov.crud_app.config.SpringConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+}
